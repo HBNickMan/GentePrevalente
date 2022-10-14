d4da1debd16d7a951de4cfa30b7229d35d1edcfb
hector.hochoc@gmail.com <115665002+HBNickMan@users.noreply.github.com>
2022-10-14 08:52:54 -0500

    Conexion Base de Datos

--- config/db.js
+++ config/db.js
@@ -1,6 +1,17 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
+
+    /*
+    //para trabajar con la base de datos en la nube
+    host: 'us-east.connect.psdb.cloud',
+    user: '5hq1jhi12k04n845pu63',
+    password: 'pscale_pw_JKhNfTAAVW6iluVmrrNllhdZTxRBGGtXMc3jXxAGIfP',
+    port: 3306,
+    database: 'gentprevdb'
+    */
+
+    //para uso y pruebas en local
     host: 'localhost',
     user: 'root',
     password: '',
