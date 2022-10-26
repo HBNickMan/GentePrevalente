2473df0edcebacc806b52f3d5cf41a7759c7d8cd
hector.hochoc@gmail.com <115665002+HBNickMan@users.noreply.github.com>
2022-10-26 15:30:12 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+            async
+            defer
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        />
       </body>
     </Html>
   )
