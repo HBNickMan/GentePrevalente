f2c754b94b86b228eb0305254c6f1bc7e018af3a
hector.hochoc@gmail.com <115665002+HBNickMan@users.noreply.github.com>
2022-10-26 14:03:55 -0500

    nuevos datos base datos

--- config/db.js
+++ config/db.js
@@ -9,14 +9,23 @@ const pool = createPool({
     password: 'pscale_pw_JKhNfTAAVW6iluVmrrNllhdZTxRBGGtXMc3jXxAGIfP',
     port: 3306,
     database: 'gentprevdb'
-    */
-
+    
     //para uso y pruebas en local
     host: 'localhost',
     user: 'root',
     password: '',
     port: 3306,
     database: 'gentprevdb'
+
+    */
+    //Nuevos datos de coneccion a la tabla
+    host: 'us-east.connect.psdb.cloud',
+    user: 'v5vtftktj9c733x1c0m9',
+    password: 'pscale_pw_Y2N6LE64l7PkCVgky7Dgpr9R0Iq3pqaaoES7JrQsUyg',
+    database: 'gentprevdb',
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 export { pool };
